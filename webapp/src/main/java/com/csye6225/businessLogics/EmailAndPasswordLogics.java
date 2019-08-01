22cceca717a8cf28fdbe3eef8bcf6ec5d1b5b685
avitiwari008 <tiwari.av@husky.neu.edu>
2019-07-31 22:30:56 -0400

    changes in webapp

--- webapp/src/main/java/com/csye6225/businessLogics/EmailAndPasswordLogics.java
+++ webapp/src/main/java/com/csye6225/businessLogics/EmailAndPasswordLogics.java
@@ -1,16 +1,32 @@
 package com.csye6225.businessLogics;
 
+import com.amazonaws.services.sns.AmazonSNSAsync;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.amazonaws.services.sns.AmazonSNSAsync;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+import com.amazonaws.services.sns.model.Topic;
+
 public class EmailAndPasswordLogics {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private AmazonSNSAsync amazonSNSClient;
+
 
     /**
      * Method to Validate is the email id is a Valid email Id
+     *
      * @param email
      * @return True/False
      */
-    public boolean ValidEmail(String email){
+    public boolean ValidEmail(String email) {
         String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 
 
@@ -22,10 +38,11 @@ public class EmailAndPasswordLogics {
 
     /**
      * Method to check if the entered Password is acceptable Password or not
+     *
      * @param password
      * @return True/False
      */
-    public boolean ValidPassword(String password){
+    public boolean ValidPassword(String password) {
         String regex = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
 
         Pattern pattern = Pattern.compile(regex);
@@ -34,4 +51,42 @@ public class EmailAndPasswordLogics {
         return matcher.matches();
     }
 
+
+    public void sendMessage(String emailId) throws ExecutionException, InterruptedException {
+
+        logger.info("Sending Message - {} ", emailId);
+
+        String topicArn = getTopicArn("password_reset");
+        PublishRequest publishRequest = new PublishRequest(topicArn, emailId);
+        Future<PublishResult> publishResultFuture = amazonSNSClient.publishAsync(publishRequest);
+        String messageId = publishResultFuture.get().getMessageId();
+
+        logger.info("Send Message {} with message Id {} ", emailId, messageId);
+
+    }
+
+    public String getTopicArn(String topicName) {
+
+        String topicArn = null;
+
+        try {
+            Topic topic = amazonSNSClient.listTopicsAsync().get().getTopics().stream()
+                    .filter(t -> t.getTopicArn().contains(topicName)).findAny().orElse(null);
+
+            if (null != topic) {
+                topicArn = topic.getTopicArn();
+            } else {
+                logger.info("No Topic found by the name : ", topicName);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        logger.info("Arn corresponding to topic name {} is {} ", topicName, topicArn);
+
+        return topicArn;
+    }
 }
--- webapp/src/main/java/com/csye6225/controller/UserController.java
+++ webapp/src/main/java/com/csye6225/controller/UserController.java
@@ -27,6 +27,8 @@ public class UserController {
 
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private EmailAndPasswordLogics emailAndPasswordLogics;
 
     @Autowired
     private MyUserDetailsService myUserDetailsService;
@@ -38,13 +40,14 @@ public class UserController {
 
     /**
      * Get method to get the Logged in User
+     *
      * @param request
      * @param response
      * @return Json
      */
-    @RequestMapping(value = "/", method= RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public String GetUser(HttpServletRequest request, HttpServletResponse response){
+    public String GetUser(HttpServletRequest request, HttpServletResponse response) {
         statsDClient.incrementCounter("endpoint.api.get");
 
         JsonObject jsonObject = new JsonObject();
@@ -52,8 +55,7 @@ public class UserController {
             jsonObject.addProperty("message", "you are logged in. current time is " + java.time.LocalTime.now().toString());
             response.setStatus(HttpServletResponse.SC_ACCEPTED);
             return jsonObject.toString();
-        }
-        catch (Exception ex){
+        } catch (Exception ex) {
             logger.info("user register");
             logger.error(ex.getMessage(), ex.getStackTrace());
             jsonObject.addProperty("error", "Exception occured! Check log");
@@ -63,6 +65,7 @@ public class UserController {
 
     /**
      * Post method to save the user in the database
+     *
      * @param request
      * @param response
      * @param user
@@ -72,38 +75,72 @@ public class UserController {
     @RequestMapping(value = "/user/register", method = RequestMethod.POST, produces = "application/json")
 
     @ResponseBody
-    public String Register(HttpServletRequest request, HttpServletResponse response, @RequestBody User user){
+    public String Register(HttpServletRequest request, HttpServletResponse response, @RequestBody User user) {
         statsDClient.incrementCounter("endpoint.user.register.api.post");
         JsonObject jsonObject = new JsonObject();
-     //   try {
-            // User user = new User();
+        //   try {
+        // User user = new User();
         logger.info("user register");
-            EmailAndPasswordLogics emailPass = new EmailAndPasswordLogics();
+        EmailAndPasswordLogics emailPass = new EmailAndPasswordLogics();
 
-            String email_id = user.getEmailAddress();
-            String password = user.getPassword();
-            // Password Validation
-            if (emailPass.ValidPassword(password)) {
-                // Email address Validation
-                if (emailPass.ValidEmail(email_id)) {
-                    // Check for already registered user
-                    User regUser = userRepository.findByEmailAddress(email_id);
-                    if (regUser == null) {
-                        myUserDetailsService.register(user);
-                        jsonObject.addProperty("message", "User Registered");
-                        response.setStatus(HttpServletResponse.SC_CREATED);
-                    } else {
-                        jsonObject.addProperty("message", "User account already exists!!!");
-                        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                    }
+        String email_id = user.getEmailAddress();
+        String password = user.getPassword();
+        // Password Validation
+        if (emailPass.ValidPassword(password)) {
+            // Email address Validation
+            if (emailPass.ValidEmail(email_id)) {
+                // Check for already registered user
+                User regUser = userRepository.findByEmailAddress(email_id);
+                if (regUser == null) {
+                    myUserDetailsService.register(user);
+                    jsonObject.addProperty("message", "User Registered");
+                    response.setStatus(HttpServletResponse.SC_CREATED);
                 } else {
-                    jsonObject.addProperty("message", "Please enter a valid Email-Id");
-                    response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
+                    jsonObject.addProperty("message", "User account already exists!!!");
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 }
             } else {
-                jsonObject.addProperty("message", "Please enter a acceptable password");
+                jsonObject.addProperty("message", "Please enter a valid Email-Id");
                 response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
             }
+        } else {
+            jsonObject.addProperty("message", "Please enter a acceptable password");
+            response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
+        }
         return jsonObject.toString();
+    }
+
+    @PostMapping(value = "/reset", produces = "application/json")
+    public String generateResetToken(@RequestBody User user, HttpServletRequest request,
+                                     HttpServletResponse response) {
+
+        statsDClient.incrementCounter("endpoint.reset.api.post");
+        logger.info("generateResetToken - Start ");
+        logger.info("email" + " " + (user.getEmailAddress()));
+        JsonObject j = new JsonObject();
+
+        try {
+            User user1 = userRepository.findByEmailAddress(user.getEmailAddress());
+            if (user1 != null) {
+                emailAndPasswordLogics.sendMessage(user1.getEmailAddress());
+                j.addProperty("message", "Password reset email sent");
+                response.setStatus(HttpServletResponse.SC_CREATED);
+
+            } else {
+                logger.info("user not present");
+                j.addProperty("Error", "Email does not exist!");
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+
+        } catch (Exception e) {
+            logger.error("Exception in generating reset token : " + e.getMessage());
+            j.addProperty("message", "Reset email failed");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        logger.info("generateResetToken - End ");
+
+        return j.toString();
+
     }
 }
